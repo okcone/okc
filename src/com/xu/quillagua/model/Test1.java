a382b316b6ec2e80bed71afc0619a29836d378e9
okcone <876317839@qq.com>
2017-05-06 14:14:36 +0800

    hello local
    hello local

--- src/com/xu/quillagua/model/Test1.java
+++ src/com/xu/quillagua/model/Test1.java
@@ -4,6 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello git");
+		System.out.println("hello local i am from response");
 	}
 
 }
